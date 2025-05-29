35bccfa5fb63bfc0249d832f9f4e9e9b2b7eccf3
skylar-rylee <sky.atacador@gmail.com>
2025-05-29 09:11:48 -0700

    commiting changes

--- BirthDateApp.java
+++ BirthDateApp.java
@@ -7,8 +7,8 @@
 // Description:
 // Asks user for their birthdate and returns the day of the week they are born
 
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class BirthDateApp
 {
@@ -61,33 +61,20 @@ public class BirthDateApp
      }  
      
      public static String getMonthName (int month) {
-          switch (month) {
-               case 1: 
-                    return "January";
-               case 2: 
-                    return "February";
-               case 3: 
-                    return "March";
-               case 4:
-                    return "April";
-               case 5:
-                    return "May";
-               case 6:
-                    return "June";
-               case 7:
-                    return "July";
-               case 8:
-                    return "August";                    
-               case 9:
-                    return "September";
-               case 10:
-                    return "October";
-               case 11:
-                    return "November";
-               case 12: 
-                    return "December";
-               default:
-                    return "Invalid";
-          }
+         return switch (month) {
+             case 1 -> "January";
+             case 2 -> "February";
+             case 3 -> "March";
+             case 4 -> "April";
+             case 5 -> "May";
+             case 6 -> "June";
+             case 7 -> "July";
+             case 8 -> "August";
+             case 9 -> "September";
+             case 10 -> "October";
+             case 11 -> "November";
+             case 12 -> "December";
+             default -> "Invalid";
+         };
      }
 }
